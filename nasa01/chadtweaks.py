aaf2bb0c706d97d3625e0116c7726a72e461d490
zouhami <zoungmida@outlook.fr>
2021-07-06 21:56:28 +0000

    sftp with UN and PW

---
+++ nasa01/chadtweaks.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import requests
+from pprint import pprint as pp # part of the standard library
+# import webbrowser
+
+## define some constants
+NASAAPI = 'https://api.nasa.gov/planetary/apod?' # this is our API to call
+MYKEY = "api_key=PAfHUOppaRFTi5mevR2XWpfAHqL6Zr5wZsTc9lq7" ## this is our api key
+
+## pretty print json
+def main():
+    """run-time code"""
+    DATE= input("Enter the desire date in format yyyy-mm-dd")
+    nasaapiobj = requests.get(NASAAPI + MYKEY + "&date=" + DATE) # call the webservice
+    nasaread = nasaapiobj.json() # parse the JSON blob returned
+
+    # Show converted json
+    print(nasaread) # show converted JSON without pprint
+    input('\nThis is converted json. Press ENTER to continue.') # pause for enter
+
+    # Show Pretty Print json
+    pp(nasaread) # this is pretty print in action
+    # pprint.pprint(convertedjson) # if you do a simple import pprint, the result is a long usage
+    input('\nThis is pretty printed JSON. Press ENTER to continue.') # pause for ENTER
+
+    # Print the description of the photo we are about to view
+    print(nasaread['explanation']) # display the value for the key explanation
+    print("Link to the APOD:", nasaread.get('hdurl',"No HD URL for today!"))
+
+
+    #input('\nPress ENTER to view this photo of the day') # pause for ENTER
+    # webbrowser.open(nasaread['hdurl']) # open in the webbrowser
+
+main()
+
