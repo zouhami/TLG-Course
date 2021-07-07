de74bc9fc37514a7950c6aaf41126f0f363f5721
zouhami <zoungmida@outlook.fr>
2021-07-07 16:48:00 +0000

    SSH and paramiko with another example of for loop script for cars

---
+++ cars.py
@@ -0,0 +1,5 @@
+ #For every [car] parked in the [garage] I'm going to [call it out].
+
+garage= ["chey truck","honda van","chevy van","dodge compact", "Audi", "Porche", "Wrenker"]
+for car in garage:
+    print(f"I found a new {car} in the garage!!")
